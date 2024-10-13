84d4c962de2c70fd106e18e02dd8df3bab8c8622
커비 <pkirby0426@gmail.com>
2024-10-13 22:58:51 +0900

    파이썬 기초
    변수와 함수(입력함수, 자료향 변환함수)

---
+++ 파이썬 기초/실습 1.py
@@ -0,0 +1,4 @@
+
+print('prof: 무엇을 도와드릴까요?')
+st = input('st:')
+print(st,'를 궁금해하시는군요')
---
+++ 파이썬 기초/이름과 주소.py
@@ -0,0 +1,6 @@
+name = input('your name:')
+address = input('address:')
+
+print('name:', name)
+print()
+print('address', address)
