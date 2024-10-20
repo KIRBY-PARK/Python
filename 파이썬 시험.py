a5f68e6de8fac9401660614fd01df1fa9342345f
커비 <pkirby0426@gmail.com>
2024-10-20 22:44:12 +0900

    파이썬 중간고사
    - 중간고사 시험볼 때 코드를 직접 확인해보고 싶은 부분이 몇가지 있어서 작성해본 코드

---
+++ 파이썬 시험.py
@@ -0,0 +1,56 @@
+
+# import random
+# print(random.randrange(1,2))
+# print(random.randint(1,2))
+
+# for i in range(1, 15, 2):
+#     print(i)
+
+# score=100
+# print('당신의 점수는' + str(score) + '입니다')
+
+# import random
+# a=[1,2,3]
+# print(a[0])
+# print(random.choice(a))
+
+# print('a'*5)
+# print(1*5)
+# print(2**2)
+# print('a', 1)
+
+# i=0
+# sum=0
+# while i < 5:
+#     sum = sum + i
+#     i = i + 1
+# print(sum)
+
+# a = 10%3//2
+# print(a)
+
+# a = 3
+
+# if a - 2 == 1:
+#     print('홀수')
+# else:
+#     print('짝수')
+
+# sum = 0
+# for i in range(10):
+#     if i%3==0 or i%7==0:
+#         sum=sum+i
+# print(sum)
+
+# a = input(10)
+# print(a+'100')
+
+# n = 1
+# while n <=5:
+#     print('hi')
+#     n=n+1
+# print('nice')
+
+a = (1+2)<3 and 1==1
+
+print(a)
